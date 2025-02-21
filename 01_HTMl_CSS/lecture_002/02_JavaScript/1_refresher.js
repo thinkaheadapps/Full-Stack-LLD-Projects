533f9580ad3fb7afa2e22ec9805f4b33f595881f
Divya Rai <thinkaheadapps@gmail.com>
2025-02-21 19:49:47 +0530

    Lecture | Full Stack LLD: JS-1: JS Refresher and Code Execution 1

---
+++ 01_HTMl_CSS/lecture_002/02_JavaScript/1_refresher.js
@@ -0,0 +1,35 @@
+var a;
+console.log("Value of a:",a);
+a = 10;
+console.log("Value of a:",a);
+a = "abc";
+console.log("value of a:",a);
+a = "abcdefgh";
+console.log("value of ");
+
+var age = 30;
+var name = "Divya";
+var c = "Hi my name is" + name + ' and age is ' + age;
+console.log("Value of c is:", c);
+
+var d = `Hi My Name is ${name}`;
+var e = `${d} and age is ${age}`
+console.log("Value of d is:", d);
+console.log("value of e is:",e);
+
+var f = "Hi\n" + 
+"Hello\n" +
+"How are you" +
+"I am good";
+
+console.log("value of f:", f);
+
+//Type of 
+console.log("Type of name",typeof(name));
+console.log("Tyope of a", typeof(a));
+console.log("type of age:", typeof(age));
+console.log("type of d", typeof(e));
+console.log("type of f", typeof(f));
+
+
+
