7c8e1653eea2d9af9417cd81de75dcee348a7992
Divya Rai <thinkaheadapps@gmail.com>
2025-02-21 21:22:53 +0530

    Full Stack LLD: JS-6: Closure and It's application|Lecture Q1 with Var

---
+++ 01_HTMl_CSS/lecture_005_Closures/Q1_1.js
@@ -0,0 +1,16 @@
+// Q1_1
+function outer() {
+  let arrFn = [];
+  for (var i = 0; i < 3; i++) {
+    arrFn.push(function fn() {
+      i++;
+      console.log(i);
+    });
+  }
+  return arrFn;
+}
+
+let arrFn = outer();
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
